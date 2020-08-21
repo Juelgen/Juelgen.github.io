c97412f6e2325924c456c35de37dcc8e9c6b7248
Andrei <annoxc@gmail.com>
2020-08-21 05:02:58 +0300

    add send data but fail

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -82,4 +82,22 @@ $(document).ready(function(){
     // mask
 
     $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+    // send data to server
+
+    $('form').submit(function(e){
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function(){
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
 });
